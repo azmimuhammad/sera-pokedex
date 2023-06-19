fe4a43b468aaa5e8062903310014a272b39f8bfc
Azmi Muhammad <azmim85@gmail.com>
2023-06-19 09:49:08 +0700

    test: fix card test

--- src/components/__test__/card.test.tsx
+++ src/components/__test__/card.test.tsx
@@ -2,13 +2,20 @@ import {render, screen} from "@testing-library/react"
 import {BrowserRouter} from "react-router-dom"
 import Card from "../card/PokemonCard"
 
-// jest.mock("./PermissionContainer", () => "This is PermissionsContainer")
 
 const item = {
   id: 6,
   name: "Bulbasaur",
   image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-  types: ["satu", "dua"]
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "fire",
+        url: "https://pokeapi.co/api/v2/type/10/"
+      }
+    }
+  ]
 }
 
 describe("Card", () => {
@@ -21,8 +28,6 @@ describe("Card", () => {
 
   test("renders app", () => {
     renderCard()
-    // screen.debug is going to print the current DOM into the console
-    // screen.debug()
     expect(screen.getByText("Bulbasaur")).toBeInTheDocument()
   })
 })
